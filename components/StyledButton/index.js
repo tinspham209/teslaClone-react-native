6fc09191f3cf9061691ed36255f89b65edbd65a8
tinspham209 <tindeptrai1@gmail.com>
2021-01-10 14:22:13 +0700

    add: StyledButton component

---
+++ components/StyledButton/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+import styles from "./styles";
+const StyledButton = ({ type, content, onPress }) => {
+	const backgroundColor = type === "primary" ? "#171a20cc" : "#ffffffA6";
+
+	const textColor = type === "primary" ? "#fff" : "#171a20";
+
+	return (
+		<View style={styles.container}>
+			<Pressable
+				style={[
+					styles.button,
+					{
+						backgroundColor: backgroundColor,
+					},
+				]}
+				onPress={() => onPress()}
+			>
+				<Text
+					style={[
+						styles.text,
+						{
+							color: textColor,
+						},
+					]}
+				>
+					{content}
+				</Text>
+			</Pressable>
+		</View>
+	);
+};
+
+export default StyledButton;
