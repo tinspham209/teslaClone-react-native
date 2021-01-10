f8ab94fcfec0ca7153d7a2caccef7a6782b86a7e
tinspham209 <tindeptrai1@gmail.com>
2021-01-10 14:22:20 +0700

    export components

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as CarItem } from "./CarItem/index";
+export { default as StyledButton } from "./StyledButton/index";
+export { default as CarsList } from "./CarsList/index";
+export { default as Header } from "./Header/index";
